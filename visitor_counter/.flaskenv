19b1cf436d4efca8124045ee89a1a5a13cfdbb18
Geek Mode <jcromerohdz@gmail.com>
2019-04-21 15:38:26 -0700

    Set up visitor counter app with postgresql 11 in flask

---
+++ visitor_counter/.flaskenv
@@ -0,0 +1,7 @@
+FLASK_APP='manage.py'
+FLASK_ENV=development
+SECRET_KEY='my_master_key'
+DB_HOST=localhost
+DB_USERNAME='flask_user'
+DB_PASSWORD='12345'
+DATABASE_NAME='counter'
---
+++ visitor_counter/settings.py
@@ -0,0 +1,9 @@
+import os
+
+SECRETE_KEY = os.getenv('SECRETE_KEY')
+DB_USERNAME = os.environ['DB_USERNAME']
+DB_PASSWORD = os.environ['DB_PASSWORD']
+DB_HOST = os.environ['DB_HOST']
+DATABASE_NAME = os.environ['DATABASE_NAME']
+DB_URI = 'postgresql://%s:%s@%s:5433/%s' %(DB_USERNAME, DB_PASSWORD, DB_HOST, DATABASE_NAME)
+SQLALCHEMY_DATABASE_URI = DB_URI
