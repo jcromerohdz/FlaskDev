79f2a4a38eba228127858ce6029501bca999efc5
Geek Mode <jcromerohdz@gmail.com>
2019-04-21 09:22:25 -0700

    Implementing static files and template inheritance with flask

--- simple_flask_app/hello.py
+++ simple_flask_app/hello.py
@@ -1,12 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# @app.route('/')
+# def hello_world():
+#     return 'Hello, World!'
+
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return render_template('index.html')
 
 @app.route('/greeting/<name>')
 def greeting(name):
-    return 'Hello ' + name
+    # return 'Hello ' + name
+    return render_template('name.html', t_name=name)
 
