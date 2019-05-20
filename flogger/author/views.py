eab33a84b5b7db2080ffebe99a1151fc644f6986
Christian Romero <jcromerohdz@gmail.com>
2019-05-20 12:20:22 -0700

    Implementing login functionality

--- flogger/author/views.py
+++ flogger/author/views.py
@@ -1,9 +1,11 @@
-from flask import Blueprint, render_template
+import sys
+
+from flask import Blueprint, render_template, redirect, session, url_for
 from werkzeug.security import generate_password_hash
 
 from application import db
 from author.models import Author
-from author.forms import RegisterForm
+from author.forms import RegisterForm, LoginForm
 
 author_app = Blueprint('author_app', __name__)
 
@@ -22,3 +24,16 @@ def register():
         db.session.commit()
         return f'Author ID: {author.id}'
     return render_template('author/register.html', form=form)
+
+@author_app.route('/login', methods=('GET', 'POST'))
+def login():
+    form = LoginForm()
+    error = None
+
+    if form.validate_on_submit():
+        author = Author.query.filter_by(email=form.email.data).first()
+        session['id'] = author.id
+        session['full_name'] = author.full_name
+        return redirect(url_for('blog_app.index'))
+
+    return render_template('author/login.html', form=form, error=error)
